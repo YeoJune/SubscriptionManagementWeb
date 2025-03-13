679c8a22e3f5e7f641bebd3aa062e7f083f6fde8
SeungHwan Kim <tmdghks12@korea.ac.kr>
2025-03-13 18:25:25 +0900

    fix route and add board placeholder (#5)

---
+++ src/pages/board.tsx
@@ -0,0 +1,14 @@
+// src/pages/board.tsx
+import './board.css';
+import React from 'react';
+
+const Board: React.FC = () => {
+  return (
+    <div className="board-hero">
+      <h1>Board</h1>
+    </div>
+  )
+}
+
+export default Board;
+
