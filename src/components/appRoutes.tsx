6b27b72d5b169cf9a75c9aac704e5537011fbd87
SeungHwan Kim <tmdghks12@korea.ac.kr>
2025-03-17 12:29:50 +0900

    add: board detail page

--- src/components/appRoutes.tsx
+++ src/components/appRoutes.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/home';
-import BoardProps from '../pages/board';
+import Board from '../pages/board';
 import NotFound from '../pages/notFound';
+import BoardDetail from '../components/board/boardDetail';
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/board" element={<BoardProps />} />
+      <Route path="/board" element={<Board />} />
+      <Route path="/board/:id" element={<BoardDetail />} />
       <Route path="*" element={<NotFound />} /> {/* 404 Not Found */}
     </Routes>
   );
--- src/pages/board.tsx
+++ src/pages/board.tsx
@@ -1,8 +1,14 @@
 // src/pages/board.tsx
 import BoardList from '../components/board/boardList';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import React, { useState } from 'react';
+import { BoardListProps, BoardProps, UserProps } from '../types';
 import './board.css';
-import React from 'react';
-import { BoardListProps, UserProps } from '../types';
 
 
 // TODO: get users list from DB
@@ -31,9 +37,44 @@ const boards: BoardListProps = {
 
 // TODO: Add a board component that will display the board list
 const Board: React.FC = () => {
+  const [selectedBoard, setSelectedBoard] = useState<BoardProps | null>(null);
+
+  const handleBoardClick = (board: BoardProps) => {
+    setSelectedBoard(board);
+  }
+
+  const handleClose = () => {
+    setSelectedBoard(null);
+  }
+
   return (
     <div className="board-container">
-      <BoardList boards={boards.boards} />
+      {/* BoardList Component에 Click Handler 전달*/}
+      <BoardList boards={boards.boards} onBoardClick={handleBoardClick} />
+
+      {/* MUI Dialog로 선택된 게시글 내용 표시 */}
+      <Dialog
+        open={Boolean(selectedBoard)}
+        onClose={handleClose}
+        fullWidth
+        maxWidth="md"
+      >
+        {selectedBoard && (
+          <>
+            <DialogTitle>{selectedBoard.title}</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                {selectedBoard.content}
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                닫기
+              </Button>
+            </DialogActions>
+          </>
+        )}
+      </Dialog>
     </div>
   )
 }
