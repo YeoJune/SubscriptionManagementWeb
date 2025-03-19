34dd97fd30ea974643a9532d82008f5376824bc4
SeungHwan Kim <tmdghks12@korea.ac.kr>
2025-03-19 13:47:58 +0900

    chore: fix routing

--- server.js
+++ server.js
@@ -8,15 +8,29 @@ const path = require("path");
 const app = express();
 const server = http.createServer(app);
 
+const session = require("express-session");
+// 공통 미들웨어
+app.use(express.json());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "default",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: process.env.NODE_ENV === "production" },
+  })
+);
 app.use(cors());
 
 // React 빌드 파일을 정적 파일로 서빙
-app.use(express.static(path.join(__dirname, "pages")));
+app.use(express.static(path.join(__dirname, "dist")));
+
+// API 라우트
+app.use("/api", require("./routes"));
 
 // React Router 사용 등 클라이언트 측 라우팅을 위해
 // 존재하지 않는 모든 요청을 React의 index.html로 보내기
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 const port = process.env.PORT || 3000;
