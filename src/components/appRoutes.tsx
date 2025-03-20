bd17bdd5246258fb8d5f967038ff0f6b4a0b4315
SeungHwan Kim <tmdghks12@korea.ac.kr>
2025-03-20 16:39:52 +0900

    add: admin users page

--- src/components/appRoutes.tsx
+++ src/components/appRoutes.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Admin from '../pages/admin';
 import Board from '../pages/board';
 import BoardDetail from '../components/board/boardDetail';
 import Home from '../pages/home';
@@ -8,6 +7,9 @@ import Login from '../pages/login';
 import NotFound from '../pages/notFound';
 import Subscription from '../pages/subscription';
 import Register from '../pages/register';
+import Users from '../components/admin/users';
+import UserInfo from '../components/admin/userInfo';
+import AdminIndex from '../pages/admin/adminIndex';
 
 const AppRoutes: React.FC = () => {
   return (
@@ -17,7 +19,9 @@ const AppRoutes: React.FC = () => {
       <Route path="/board/:id" element={<BoardDetail />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/admin" element={<Admin />} />
+      <Route path="/admin" element={<AdminIndex />} />
+      <Route path="/admin/users" element={<Users />} />
+      <Route path="/admin/users/:id" element={<UserInfo />} />
       <Route path="/subscription" element={<Subscription />} />
       <Route path="*" element={<NotFound />} /> {/* 404 Not Found */}
     </Routes>
---
+++ src/pages/admin/adminIndex.tsx
@@ -0,0 +1,12 @@
+// src/pages/admin/adminIndex.tsx
+import React from 'react';
+
+const AdminIndex: React.FC = () => {
+  return (
+    <div>
+      <h1>Admin Index</h1>
+    </div>
+  );
+};
+
+export default AdminIndex;
