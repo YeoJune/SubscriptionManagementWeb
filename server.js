b2735d9237980874c1e6aeeb9ade2dabc8c95295
YeoJoon <joyyoj1@naver.com>
2025-06-27 02:49:08 +0900

    not https

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === 'production',
+      secure: false,
       maxAge: 24 * 60 * 60 * 1000, // 24시간
     },
   })
